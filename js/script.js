259e6ae6b5ab15526d8d0430e70c99221f667f92
juandapps <juandapps@hotmail.com>
2023-04-18 13:31:49 -0500

    Add test hard reset

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import projectsData from "./components/projectsData.js";
 import mobileMenu from "./components/mobileMenu.js";
 
-// eslint-disable-next-line
+// eslint-disable-next-lin
 const data = new projectsData();
 const menu = new mobileMenu("[data-menu]", "[data-overlay]");

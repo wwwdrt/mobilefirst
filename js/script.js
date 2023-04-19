cb360e1d89dc37af8b6beb0b1535e0ee7d4f12ef
juandapps <juandapps@hotmail.com>
2023-04-18 23:39:16 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 // eslint-disable-next-line import/extensions
-import Projects from './components/Projects.js';
+import Projects from "./components/Projects.js";
 // eslint-disable-next-line max-len
 // eslint-disable-next-line import/no-named-as-default,import/no-named-as-default-member,import/extensions
-import Menu from './components/Menu.js';
+import Menu from "./components/Menu.js";
+
+// eslint-disable-next-line max-len
+// eslint-disable-next-line import/no-named-as-default,import/no-named-as-default-member,import/extensions
+import Popup from "./components/Popup.js";
 
 // eslint-disable-next-line new-cap,no-new
 new Projects();
 // eslint-disable-next-line no-new,new-cap
-new Menu('[data-menu]', '[data-overlay]');
+new Menu("[data-menu]", "[data-overlay]");
+
+// eslint-disable-next-line new-cap,no-new
+new Popup();

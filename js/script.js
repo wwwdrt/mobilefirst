a594f9b65dba2683cfebd56621f33a6aeb315cf7
juandapps <juandapps@hotmail.com>
2023-04-21 14:32:12 -0500

    Add local storage validation

--- js/script.js
+++ js/script.js
@@ -11,6 +11,10 @@ import Popup from './components/Popup.js';
 // eslint-disable-next-line import/no-named-as-default,import/no-named-as-default-member
 import Contact from './components/Contact.js';
 
+// eslint-disable-next-line max-len
+// eslint-disable-next-line import/no-named-as-default,import/no-named-as-default-member,import/no-unresolved
+import Storage from './components/Storage.js';
+
 // eslint-disable-next-line new-cap,no-new
 new Projects();
 // eslint-disable-next-line no-new,new-cap
@@ -21,3 +25,6 @@ new Popup();
 
 // eslint-disable-next-line no-new
 new Contact();
+
+// eslint-disable-next-line no-new
+new Storage();

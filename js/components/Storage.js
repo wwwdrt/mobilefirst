4b7e047cfcf7098e411becf647f14c1008b9385f
juandapps <juandapps@hotmail.com>
2023-04-21 16:17:05 -0500

    Update requested console.log changes

--- js/components/Storage.js
+++ js/components/Storage.js
@@ -1,59 +1,8 @@
 class Storage {
   constructor() {
-    // Check for localStorage & sessionStorage
-    this.isLocalStorageAvailable();
-    this.isSessionStorageAvailable();
-
     // Set and Get localStorage
     this.storeLocalData();
   }
-
-  validateStorage = (type) => {
-    let storage;
-    try {
-      storage = window[type];
-      const x = '__storage_test__';
-      storage.setItem(x, x);
-      storage.removeItem(x);
-      return true;
-    } catch (e) {
-      return (
-        e instanceof DOMException
-        // everything except Firefox
-        && (e.code === 22
-          // Firefox
-          || e.code === 1014
-          // test name field too, because code might not be present
-          // everything except Firefox
-          || e.name === 'QuotaExceededError'
-          // Firefox
-          || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
-        // acknowledge QuotaExceededError only if there's something already stored
-        && storage
-        && storage.length !== 0
-      );
-    }
-  };
-
-  isLocalStorageAvailable = () => {
-    if (this.validateStorage('localStorage')) {
-      // eslint-disable-next-line no-console
-      console.log('Yippee! We can use localStorage awesomeness');
-    } else {
-      // eslint-disable-next-line no-console
-      console.log('Too bad, no localStorage for us');
-    }
-  };
-
-  isSessionStorageAvailable = () => {
-    if (this.validateStorage('sessionStorage')) {
-      // eslint-disable-next-line no-console
-      console.log('Yippee! We can use sessionStorage awesomeness');
-    } else {
-      // eslint-disable-next-line no-console
-      console.log('Too bad, no sessionStorage for us');
-    }
-  };
 
   storeLocalData = () => {
     const form = document.querySelector('form');
